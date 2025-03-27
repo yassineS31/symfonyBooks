e5ebbc102f2808e812117128a377cc5e517822f9
yassine <y31m3x@gmail.com>
2025-03-27 12:30:49 +0100

    fix cypress

--- cypress/e2e/addCategory.cy.js
+++ cypress/e2e/addCategory.cy.js
@@ -2,7 +2,10 @@ describe('template spec', () => {
     it('passes', () => {
       cy.visit('https://127.0.0.1:8000/category/new')
       cy.get('#category_label').type('Roman');
-      cy.get('.btn').click();
+      cy.get('.btn').click().wait(1000);
+      // Verification de redirection
+      cy.get('h1').contains('Category index');
+
       
     })
   })
